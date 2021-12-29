086fd58a1149c29aaab263ea30019e0b9cc196c2
Valerie Sum <341116028@gapps.yrdsb.ca>
2021-12-28 22:20:13 -0500

    added text class and start button

--- StartPage.java
+++ StartPage.java
@@ -8,14 +8,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class StartPage extends World
 {
-
+    Text start = new Text(" Press to Start ", 50, 255, 255, 255, 70, 70, 70); 
+    
     /**
      * Constructor for objects of class StartPage.
      * 
      */
     public StartPage()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 600, 1); 
+        // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1);
+        addObject(start, 180, 540);
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(start))
+        {
+            System.out.println("hi");
+        }
     }
 }
---
+++ Text.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Displays text.
+ * 
+ * @author Valerie 
+ * @version Dec. 28/21
+ */
+public class Text extends Actor
+{
+    /**
+     * Creates text image.
+     * Based off https://www.youtube.com/watch?v=NjP3MMJnvGc
+     * 
+     * @param content The sentences that are wanted to be displayed.
+    */
+    public Text(String content, int size, int r, int g, int b, int br, int bg, int bb) 
+    {
+        Color textColor = new Color(r, g, b);
+        Color background = new Color(br, bg, bb);
+        setImage(new GreenfootImage(content, size, textColor, background));
+    }   
+}

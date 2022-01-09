747358d2c3d4fdcc06fd4460ccc2c6cc63cb48a9
davvd123 <335671335@gapps.yrdsb.ca>
2022-01-09 10:28:06 -0500

    textchoice changes

--- TextChoice.java
+++ TextChoice.java
@@ -8,12 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TextChoice extends Actor
 {
-    /**
-     * Act - do whatever the TextChoice wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        // Add your action code here.
+    private String theText;
+    private boolean choice;
+    public TextChoice(String text, boolean positive){
+        theText = text;
+        choice = positive;
+    }
+    public String getText(){
+        return theText;
+    }
+    public boolean getChoice(){
+        return choice;
     }
 }

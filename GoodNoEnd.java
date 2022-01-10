772502da2b64a5dda51bd9a0470a579bd61a94f5
Valerie Sum <341116028@gapps.yrdsb.ca>
2022-01-09 23:02:35 -0500

    endings

--- GoodNoEnd.java
+++ GoodNoEnd.java
@@ -1,21 +1,31 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class GoodNoEnd here.
+ * Ending for if the player decides to befriend Mary.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Valerie 
+ * @version Jan. 9/22
  */
 public class GoodNoEnd extends World
 {
-
+    String dialogue = "Within a span of a year, Mary and I have become \n" +
+                      "close friends. Knowing a spirit has its perks. \n\n" +
+                      "We talk every few days and have a blast. I give \n" +
+                      "her ideas on how to scare people and she supports \n" +
+                      "me through my dating struggles. I may not \n" +
+                      "have met the one, but I'm glad I've. \n" +
+                      "found someone who'll always be there for me. \n";
+    
+    Text endText = new Text(dialogue, 30, 255, 255, 255, 0, 0, 0);
+    
     /**
      * Constructor for objects of class GoodNoEnd.
      * 
      */
     public GoodNoEnd()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1);
+        addObject(endText, 400, 300); 
     }
 }
--- GoodYesEnd.java
+++ GoodYesEnd.java
@@ -1,21 +1,30 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class GoodYesEnd here.
+ * Ending for if the user decides to date Mary.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class GoodYesEnd extends World
 {
-
+    String dialogue = "2 years later, Mary and I are still going strong. \n" +
+                      "I talk to her through the mirror everyday and \n" +
+                      "spend time with her. \n\n" +
+                      "We can't predict the future of our relationship, \n" + 
+                      "but I enjoy every moment I spend with her. \n" +
+                      "As long as we're both here for each other, I \n" + 
+                      "think we'll be just fine.";
+    
+    Text endText = new Text(dialogue, 30, 255, 255, 255, 0, 0, 0);
     /**
      * Constructor for objects of class GoodYesEnd.
      * 
      */
     public GoodYesEnd()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1);
+        addObject(endText, 400, 300);
     }
 }

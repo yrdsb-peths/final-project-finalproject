3131d2d787cc75da24e2c5efb29b662d2878815c
davvd123 <335671335@gapps.yrdsb.ca>
2022-01-11 09:46:55 -0500

    texts

--- TextChoice.java
+++ TextChoice.java
@@ -8,33 +8,26 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TextChoice 
 {
-    private String maryQpos;
-    private String maryQneg;
-    private String res1;
-    private String res2;
-    private String res3;
-    private String maryR1pos;
-    private String maryR1neg;
-    private String maryR2pos;
-    private String maryR2neg;
-    private String maryR3pos;
-    private String maryR3neg;
+    private String mary1;
+    private String playerResponse;
+    private String maryResponse;
     private boolean choice;
-    public TextChoice(String text, String response, boolean positive){
-        theText = text;
-        choice = positive;
-        
+    public TextChoice(String mary1pos, String mary1neg, String res1, String res2, String res3, String mRes1pos, String mRes1neg, String mRes2pos, String mRes2neg, String mRes3pos, String mRes3neg, boolean positive){
+        String[] posOption = new String[2];
+        String[] nulOption = new String[2];
+        String[] negOption = new String[2];
+        if (positive){
+            mary1 = mary1pos;
+        } else {
+            mary1 = mary1neg;
+        }     
     }
-    private void points (String type){
-        
-    }
+
     public String getText(){
-        return theText;
+        return mary1;
     }
-    public boolean getChoice(){
-        return choice;
-    }
+
     public String getResponse1(){
-        return theResponse;
+        return playerResponse;
     }
 }

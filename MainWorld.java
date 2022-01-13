b628e2b89ba3e66546d30a2e6737c1183adb7ae4
Valerie Sum <341116028@gapps.yrdsb.ca>
2022-01-12 22:43:31 -0500

    change backgrounds method

--- MainWorld.java
+++ MainWorld.java
@@ -9,20 +9,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MainWorld extends World
 {
     Integer romanceScore = 0;
+    String name;
     PlayerTextBox playerBox = new PlayerTextBox();
+    MaryTextBox maryBox = new MaryTextBox();
     
-    // Player summons Mary.
+    // Text for when player summons Mary in the beginning of the game.
     Text ritual1 = new Text(" Well this is getting depressing. Who thought \n" +
                             "summoning a spirit was the last resort to curing \n" +
                             "loneliness. Whatever. Let me go turn off the light. ",
                             25);
 
     Text ritual2 = new Text(" Ok, here I go. Bloody Mary, bloody Mary, \n" +
-                            "bloody Mary� ", 30);
+                            "bloody Mary ", 30);
     
     /**
      * Constructor for objects of class MainWorld.
-     * 
      */
     public MainWorld()
     {    
@@ -33,6 +34,30 @@ public class MainWorld extends World
     public void act()
     {
         beginRitual();
+        changeMary();
+        prompt1();
+    }
+    
+    /**
+     * Changes Mary's reaction depending on player's score.
+     */
+    public void changeMary()
+    {
+        if(romanceScore < 70) { // Super upset reaction.
+            setBackground("super upset mary.png");
+        }
+        else if(romanceScore < 100 && romanceScore > 60) { // Upset reaction.
+            setBackground("upset mary.png");
+        }
+        else if(romanceScore < 140 && romanceScore > 100) { // Happy reaction.
+            setBackground("happy mary.png");
+        }
+        else if(romanceScore > 130) { // Super happy reaction.
+            setBackground("super happy mary.png");
+        }
+        else { // Neutral reaction.
+            setBackground("mary.png");
+        }
     }
     
     public void beginRitual()

9e0eee7005c9abd2eeab2226c286abbed7ea4b21
davvd123 <335671335@gapps.yrdsb.ca>
2022-01-13 10:25:30 -0500

    moved text file code into mainworld

--- MainWorld.java
+++ MainWorld.java
@@ -1,5 +1,11 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.*;
+import java.util.*;
+import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 /**
  * Main portion of the game.
  * 
@@ -76,5 +82,38 @@ public class MainWorld extends World
     
     public void prompt1()
     {
+    }
+    
+    public void generateText() 
+    throws FileNotFoundException
+    {
+        String questionPath = "C:\\Users\\david\\OneDrive\\Documents\\GitHub\\final-project-finalproject\\TextFiles\\Questions.txt";
+        String responsePath = "C:\\Users\\david\\OneDrive\\Documents\\GitHub\\final-project-finalproject\\TextFiles\\Responses.txt";;
+        String reactionPath = "C:\\Users\\david\\OneDrive\\Documents\\GitHub\\final-project-finalproject\\TextFiles\\Reactions.txt";;
+        
+        ArrayList<String> questions = new ArrayList<String>(); //question 0 associated with response 0
+        ArrayList<String> responses = new ArrayList<String>();
+        ArrayList<String> reactions = new ArrayList<String>();
+        
+        readPath(questions, questionPath);
+        readPath(responses, responsePath);
+        readPath(reactions, reactionPath);
+    }
+    private void readPath(ArrayList<String> temp, String path) throws FileNotFoundException{
+        InputStream is = new FileInputStream(path);
+ 
+        // Try block to check for exceptions
+        try (Scanner sc = new Scanner(
+                 is, StandardCharsets.UTF_8.name())) {
+ 
+            // It holds true till there is single element
+            // left in the object with usage of hasNext()
+            // method
+            while (sc.hasNextLine()) {
+ 
+                // Printing the content of file
+                temp.add(sc.nextLine());
+            }
+        }
     }
 }
